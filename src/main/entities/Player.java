ab3b544293ece6bcf98b95c908fb37f6ed165788
Poulpi_ <1312.maxou4ever@gmail.com>
2022-05-14 12:54:31 +0200

    Corrections in Player.java

--- src/main/entities/Player.java
+++ src/main/entities/Player.java
@@ -1,5 +1,6 @@
 package main.entities;
 
+import main.locations.Castle;
 import main.locations.Room;
 import main.objects.*;
 
@@ -24,10 +25,59 @@ public class Player extends Entity {
     }
 
 
+    /**
+     * This method permits to get the player's weapon
+     * @return the player's weapon
+     */
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    /**
+     * This method permits to set the player's weapon
+     * @param weapon the player's weapon
+     */
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    /**
+     * This method permits to get the player's room
+     * @return the player's room
+     */
     public Room getRoom() {
         return room;
     }
+
+    /**
+     * This method permits to set the player's room
+     * @param room the player's room
+     */
     public void setRoom(Room room) {
         this.room = room;
+    }
+
+    /**
+     * This method permits to place the player at the enter of the castle.
+     * @param castle the castle
+     */
+    public void spawn(Castle castle) {
+        Room[][] rooms  = castle.getFloors()[0].getRooms();
+        for (Room[] row : rooms) {
+            for (Room room : row) {
+                if (room.isSpawn()) {
+                    this.room = room;
+                    return;
+                }
+            }
+        }
+    }
+
+    /**
+     * This method show player's stats
+     */
+    public void showStats() {
+        System.out.println("Information about your character :");
+        System.out.println("HP: " + this.getPv() + " - DMG: " + this.getDamage());
     }
 }
