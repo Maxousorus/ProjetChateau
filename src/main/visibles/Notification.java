de9f9d512e09e1c1e1f690685af54d5ca7da7125
Poulpi_ <1312.maxou4ever@gmail.com>
2022-05-19 21:12:04 +0200

    Create CanBeInPassage interface and Notification class

---
+++ src/main/visibles/Notification.java
@@ -0,0 +1,31 @@
+package main.visibles;
+
+/**
+ * This class permits to display a notification.
+ *
+ * @author BOUDIER Maxime; BAYEN MAXIME; FOURNIER Victor; DOSSA Josias.
+ * @see Menu Extends of Menu
+ */
+public class Notification extends Menu{
+
+    public static final String[] OK = {"Press enter"}; // The text of the button.
+
+    /**
+     * Constructor of Notification.
+     * @param title Title of the notification.
+     * @param map map of the notification.
+     * @see Map
+     */
+    public Notification(String title, Map map) {
+        super(title, OK, map);
+    }
+
+    /**
+     * Constructor of Notification.
+     * @param title Title of the notification.
+     */
+
+    public Notification(String title){
+        this(title, null);
+    }
+}
