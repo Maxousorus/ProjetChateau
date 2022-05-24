858c7e0c37dc58be82e1acd844999d2b2bdc7289
Poulpi_ <1312.maxou4ever@gmail.com>
2022-05-24 18:04:37 +0200

    Create Main menu

--- src/main/game/Main.java
+++ src/main/game/Main.java
@@ -1,5 +1,9 @@
 package main.game;
 
+import main.utils.Parameters;
+import main.visibles.Menu;
+import main.visibles.Notification;
+
 import java.io.IOException;
 
 /**
@@ -14,10 +18,46 @@ public class Main {
      * @throws IOException the io exception
      */
     public static void main(String[] args) throws IOException {
-         Game game = new Game();
 
-         //TODO Menu principale (lancer le jeu, paramètres, credits, quitter)
+        Parameters.initParameters();
 
-         game.run();
+        Game game;
+        Game previous_game = null;
+
+        boolean quit = false;
+        while(!quit) {
+            Menu menu;
+            if(previous_game == null) {
+                menu = new Menu("Welcome brave warrior !", new String[]{"Start", "Settings", "Credits", "Quit"});
+
+                switch (menu.choose()) {
+                    case 0 -> {
+                        game = new Game();
+                        previous_game = game;
+                        game.run();
+                    }
+                    case 1 -> Parameters.parametersMenu();
+                    case 2 -> new Notification("This game has been created by : BOUDIER Maxime, BAYEN Maxime, FOURNIER Victor, DOSSA Josias !").choose();
+                    case 3 -> quit = true;
+                }
+            }else {
+                menu = new Menu("Welcome back !", new String[]{"Restart in a new Castle", "Visit previous castle", "Settings", "Credits", "Quit"});
+
+                switch (menu.choose()) {
+                    case 0 -> {
+                        game = new Game();
+                        previous_game = game;
+                        game.run();
+                    }
+                    case 1 -> {
+                        previous_game.run();
+                    }
+                    case 2 -> Parameters.parametersMenu();
+                    case 3 -> new Notification("This game has been created by : BOUDIER Maxime, BAYEN Maxime, FOURNIER Victor, DOSSA Josias !").choose();
+                    case 4 -> quit = true;
+                }
+            }
+
+        }
     }
 }
