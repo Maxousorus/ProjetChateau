5ad739fb28201073d7395609ee6ad72a95ed8776
Poulpi_ <1312.maxou4ever@gmail.com>
2022-05-24 18:05:47 +0200

    Make JavaDoc friendly and add colors

--- src/main/visibles/Map.java
+++ src/main/visibles/Map.java
@@ -15,9 +15,9 @@ public class Map {
 
     private final Player player; // The player of the map.
 
-    private final String sidewall = "▓▓"; //Wall on side (left and right) of the map.
+    private final String sidewall = "\u2593\u2593"; //Wall on side (left and right) of the map.
 
-    private final String aroundPassage = "▓▓▓▓"; //Wall around the passage and between rooms
+    private final String aroundPassage = "\u2593\u2593\u2593\u2593"; //Wall around the passage and between rooms
 
     /**
      * Constructor of the class Map.
@@ -56,7 +56,7 @@ public class Map {
     private String updownwall() { //Wall on top and bottom of the map.
         String room = "";
         for (int i = 0; i < Parameters.ROOM_SIZE; i++) {
-            room += "▓";
+            room += "\u2593";
         }
         String updownwall = sidewall;
         for (int i = 0; i < Parameters.FLOOR_SIZE; i++) {
@@ -84,14 +84,16 @@ public class Map {
     public String toString() {
         Floor floor = player.getRoom().getFloor();
         Room[][] rooms = floor.getRooms();
-        String map = updownwall() + "\n";//Add the top wall
+        String map = Parameters.MAP_COLOR; //Color of the map.
+        map += updownwall() + "\n";//Add the top wall
 
         for (int row = 0; row < Parameters.FLOOR_SIZE; row++) { //For each row of rooms
             for (int nbline = 0; nbline < Parameters.ROOM_HEIGHT; nbline++) { //for each line of the row of rooms
-                String line = "";
+                String line = Parameters.MAP_COLOR;
                 line += sidewall; //Put sidewall on the left of the line
                 for (int col = 0; col < Parameters.FLOOR_SIZE; col++) { //For each room of the column
                     line += rooms[row][col].toStringList(player).get(nbline); //Add the line of the room
+                    line += Parameters.MAP_COLOR;
                     if (col < Parameters.FLOOR_SIZE - 1) { //If it's not the last room of the column
                         Passage[][] h_passages = floor.getHorizontal_passages();
                         if (h_passages[row][col] != null) { //If there is a passage between the room and the next
@@ -99,13 +101,13 @@ public class Map {
                             if (Parameters.ROOM_HEIGHT % 2 == 1) { //If the number of lines of the passage is not pair
                                 if (nbline >= (Parameters.ROOM_HEIGHT / 2) - 1 && nbline <= (Parameters.ROOM_HEIGHT / 2) + 1) { //If the line of the passage is in the middle of the passage
                                     line += passage.get(nbline - ((Parameters.ROOM_HEIGHT / 2) - 1)); //Add the line of the passage
-                                } else line += "▓▓▓▓"; //If the line of the passage is not in the middle of the passage, put a wall
+                                } else line += "\u2593\u2593\u2593\u2593"; //If the line of the passage is not in the middle of the passage, put a wall
                             } else if (Parameters.ROOM_HEIGHT % 2 == 0) { //If the number of lines of the passage is pair
                                 if (nbline >= (Parameters.ROOM_HEIGHT / 2) - 2 && nbline <= (Parameters.ROOM_HEIGHT / 2) + 1) { //If the line of the passage is in the middle of the passage
                                     line += passage.get(nbline - ((Parameters.ROOM_HEIGHT / 2) - 2)); //Add the line of the passage
-                                } else line += "▓▓▓▓";  //If the line of the passage is not in the middle of the passage, put a wall
+                                } else line += "\u2593\u2593\u2593\u2593";  //If the line of the passage is not in the middle of the passage, put a wall
                             } else { //If the number of lines of the passage is not a number
-                                line += "▓▓▓▓"; //If the number of lines of the passage is not even, put a wall
+                                line += "\u2593\u2593\u2593\u2593"; //If the number of lines of the passage is not even, put a wall
                             }
                         } else { //If there is no passage between the room and the next
                             line += aroundPassage; //If there is no passage between the room and the next, put a wall
@@ -122,17 +124,17 @@ public class Map {
                     for (int col = 0; col < Parameters.FLOOR_SIZE; col++) { //For each room of the column
                         if (v_passages[row][col] == null) { //If there is no passage between the room and the next
                             for (int j = 0; j < Parameters.ROOM_SIZE; j++) { //For each line of the room
-                                line += "▓"; //If there is no passage between the room and the next, put a wall
+                                line += "\u2593"; //If there is no passage between the room and the next, put a wall
                             }
                         } else { //If there is a passage between the room and the next
                             String nextToPassage = "";//Add the line of the passage after the middle of the passage
                             if (Parameters.ROOM_SIZE % 2 == 1) { //If the number of lines of the passage is not pair
                                 for (int j = 0; j < (Parameters.ROOM_SIZE / 2) - 1; j++) { //For each line of the passage before the middle of the passage
-                                    nextToPassage += "▓"; //Put a wall
+                                    nextToPassage += "\u2593"; //Put a wall
                                 }
                             } else {
                                 for (int j = 0; j < (Parameters.ROOM_SIZE / 2) - 2; j++) { //For each line of the passage before the middle of the passage
-                                    nextToPassage += "▓"; //Put a wall
+                                    nextToPassage += "\u2593"; //Put a wall
                                 }
                             }
                             line += nextToPassage; //Add the line of the passage before the middle of the passage
@@ -140,7 +142,7 @@ public class Map {
                             line += nextToPassage; //Add the line of the passage after the middle of the passage
                         }
                         if (col < Parameters.FLOOR_SIZE - 1) { //If it's not the last room of the column
-                            line += "▓▓▓▓"; //Put a wall between the room and the next
+                            line += "\u2593\u2593\u2593\u2593"; //Put a wall between the room and the next
                         } else line += sidewall; //Put sidewall on the right of the line
                     }
                     map += line + "\n"; //Add the line
@@ -149,6 +151,7 @@ public class Map {
                 map += updownwall(); //Add the last line of the floor
             }
         }
+        map += Parameters.RESET_COLOR;
     return map;
     }
 }
