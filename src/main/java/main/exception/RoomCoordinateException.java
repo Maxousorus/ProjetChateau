12c94134574c4c008e4addd642d3cbf3d901831d
Poulpi_ <1312.maxou4ever@gmail.com>
2022-06-06 23:16:01 +0200

    The Real Final code !

--- src/main/java/main/exception/RoomCoordinateException.java
+++ src/main/java/main/exception/RoomCoordinateException.java
@@ -1,6 +1,12 @@
 package main.exception;
 
+/**
+ * The type Room coordinate exception.
+ */
 public class RoomCoordinateException extends Exception {
+    /**
+     * Instantiates a new Room coordinate exception.
+     */
     public RoomCoordinateException() {
         super("RoomCoordinateException");
     }
